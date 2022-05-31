d56e47ec4da59d08c12308be843afbcf626837bb
timothyliang <90925timmy@gmail.com>
2022-05-31 16:12:20 +0800

    timothy test

--- assets/Scripts/bullet.ts
+++ assets/Scripts/bullet.ts
@@ -4,7 +4,7 @@
 //  - https://docs.cocos.com/creator/manual/en/scripting/reference/attributes.html
 // Learn life-cycle callbacks:
 //  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
-
+//try
 const { ccclass, property } = cc._decorator;
 
 @ccclass
