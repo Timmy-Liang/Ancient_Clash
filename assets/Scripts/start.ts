a7755de936139d3b3cd2eebb336b0c6428277ade
timothyliang <90925timmy@gmail.com>
2022-06-02 13:22:52 +0800

    test

--- assets/Scripts/start.ts
+++ assets/Scripts/start.ts
@@ -10,12 +10,29 @@ const {ccclass, property} = cc._decorator;
 @ccclass
 export default class start extends cc.Component {
 
-    
-
+    @property(cc.Node)
+    select: cc.Node = null;
+    @property(cc.Node)
+    shade1: cc.Node = null;
+    @property(cc.Node)
+    shade2: cc.Node = null;
     start () {
 
     }
-    gamestart2V2(){
+    changetoselect2V2(){
+        this.select.active=true;
+    }
+    playerswitch1(customEventData: string){ 
+        this.shade1.active=true;
+        this.shade2.active=false;
+    
+    }
+    playerswitch2(){
+        this.shade1.active=false;
+        this.shade2.active=true;
+        
+    }
+    gamestart(){
         cc.director.loadScene("game");
     }
 
