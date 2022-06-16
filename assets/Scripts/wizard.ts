f7c5097f64b112aca48a15017bd26c82302c168f
Mandy <mandy1030136@gmail.com>
2022-06-16 17:32:55 +0800

    revise wizard

--- assets/Scripts/wizard.ts
+++ assets/Scripts/wizard.ts
@@ -6,70 +6,57 @@ export default class wizard extends cc.Component {
   targetRegion: cc.Prefab = null;
   @property(cc.Prefab)
   fire: cc.Prefab = null;
+  @property(cc.Node)
+  player: cc.Node = null;
+  @property(cc.Node)
+  map: cc.Node = null;
 
-  private wizardLeft;
-  private wizardRight;
-
-  coolTime: number = 10;
-  range: number = 3; //recommand: 1 <= range <= 6
-  die1: boolean = false;
-  die2: boolean = false;
+  private wizardLife: number = 10;
+  private targetRange: number = 3; //recommand : 1<= range <= 6
+  private targetGenerate;
 
   // onLoad () {}
 
   start() {
-    this.wizardLeft = () => {
-      this.generateTargetRegion(false);
-    };
-    this.wizardRight = () => {
-      this.generateTargetRegion(true);
+    this.targetGenerate = () => {
+      this.generateTargetRegion();
     };
 
-    this.schedule(this.wizardLeft, 10);
-    this.schedule(this.wizardRight, 10);
+    this.schedule(this.targetGenerate, 10);
   }
 
-  generateTargetRegion(dir: boolean) {
-    let targetL = cc.instantiate(this.targetRegion);
+  generateTargetRegion() {
+    let target = cc.instantiate(this.targetRegion);
     let pos;
-    targetL.scale = this.range;
-    if (dir == false) {
-      targetL.parent = cc.find("Canvas/map1_1");
-    } else {
-      targetL.parent = cc.find("Canvas/map1_2");
-    }
-    let choosenX = 1655 - (this.range - 1) * 224;
-    let choosenY = 1775 - (this.range - 1) * 160;
+    target.scale = this.targetRange;
+    target.parent = this.map;
+    
+    let choosenX = 1655 - (this.targetRange- 1) * 224;
+    let choosenY = 1775 - (this.targetRange - 1) * 160;
     pos = cc.v2(
-      130 + (this.range - 1) * 112 + Math.floor(Math.random() * choosenX),
-      60 + (this.range - 1) * 80 + Math.floor(Math.random() * choosenY)
+      130 + (this.targetRange - 1) * 112 + Math.floor(Math.random() * choosenX),
+      60 + (this.targetRange - 1) * 80 + Math.floor(Math.random() * choosenY)
     );
-    targetL.setPosition(pos);
+    target.setPosition(pos);
 
     this.scheduleOnce(() => {
-      this.explosion(dir, pos);
-      targetL.destroy();
+      this.explosion(pos);
+      target.destroy();
     }, 4.8);
   }
-  explosion(mapIndex: boolean, pos: number) {
+  explosion(pos: number) {
     let fire = cc.instantiate(this.fire);
-    fire.scale = this.range;
-    if (mapIndex == false) {
-      fire.parent = cc.find("Canvas/map1_1");
-    } else {
-      fire.parent = cc.find("Canvas/map1_2");
-    }
+    fire.scale = this.targetRange;
+    fire.parent = this.map;
+    
     fire.setPosition(pos);
     this.scheduleOnce(() => {
       fire.destroy();
     }, 3.2);
   }
   update(dt) {
-    if (this.die1) {
-      this.unschedule(this.wizardLeft);
-    }
-    if (this.die2) {
-      this.unschedule(this.wizardRight);
+    if (this.wizardLife <= 0) {
+       this.unschedule(this.targetGenerate);
     }
   }
 }
