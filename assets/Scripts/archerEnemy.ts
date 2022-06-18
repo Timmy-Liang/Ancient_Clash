796d2f50f6deb7b491c50b31fce727a9ba206e4d
danny43861 <danny43861@gmail.com>
2022-06-18 11:29:27 +0800

    Half of archerEnemy

--- assets/Scripts/archerEnemy.ts
+++ assets/Scripts/archerEnemy.ts
@@ -5,12 +5,11 @@
 // Learn life-cycle callbacks:
 //  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
 
-import player from './player'
-import pathFinding from './pathFinding'
-const { ccclass, property } = cc._decorator;
+const {ccclass, property} = cc._decorator;
 
 @ccclass
-export default class meleeEnemy extends cc.Component {
+export default class archerEnemy extends cc.Component {
+
     @property(cc.Node)
     gameManager: cc.Node = null;
 
@@ -19,15 +18,9 @@ export default class meleeEnemy extends cc.Component {
     private detectRange: number = 200;
     private attackRange: number = 5;
     private attackCooldown: number = 1;
-    private moveSpeed: number = 40;
-    public tracingPlayer: boolean = false;
-
-    private setPathCooldown: number = 0.5;
-    private traceCooldown: number = 0.1;
-    private traceSpeed: number = 160;
-    private traceDifficulty: number = 1;
-    private tracePath: Array<Array<number>> = [];
-
+    private aiming: boolean = false;
+    
+    private moveSpeed: number = 70;
     private moveDir: cc.Vec2 = cc.Vec2.ZERO;
     private moveDuration: number = 2.0;
     private waitDuration: number = 2;
@@ -65,7 +58,6 @@ export default class meleeEnemy extends cc.Component {
         if (currentTime >= this.nextMoveTime) {
             this.nextWaitTime = currentTime + this.moveDuration;
             this.nextMoveTime = this.nextWaitTime + this.waitDuration * (1.0 + this.waitRandomFactor * (Math.random() * 2.0 - 1.0));
-            this.moveDir = randomPointOnUnitCircle();
         }
         if (currentTime < this.nextWaitTime) {
             this.node.x += this.moveDir.x * this.moveSpeed * dt;
@@ -73,20 +65,8 @@ export default class meleeEnemy extends cc.Component {
         }
     }
 
-    setPath() {
-        let tmp = [];
-        let targetPos: cc.Vec2 = cc.v2(0, 0)
-        if (this.tracingPlayer) {
-            targetPos = this.target.convertToWorldSpaceAR(cc.v2(0, 0));
-        }
-        this.tracePath = this.node.getComponent('pathFinding').pathFindingAlgo(targetPos);
-
-    }
-
     detectRangePlayer() {
-        //console.log(this.node.position.sub(this.player1.node.position).mag())
         if (this.node.convertToWorldSpaceAR(cc.v2(0, 0)).sub(this.target.convertToWorldSpaceAR(cc.v2(0, 0))).mag() < this.detectRange) {
-            this.moveSpeed = 200;
             this.tracingPlayer = true;
         }
         return 0;
@@ -180,21 +160,4 @@ export default class meleeEnemy extends cc.Component {
             }
         }
     }
-
-    onPreSolve(contact, self, other) {
-        if(this.attacking)
-            return;
-        if(other.node.name == 'player'){
-            this.enemyAttackAnimation();
-            other.node.getComponent(player).lifeDamage(1);
-        }
-    }
-
-
 }
-
-
-function randomPointOnUnitCircle() {
-    let angle = Math.random() * Math.PI * 2;
-    return new cc.Vec2(Math.cos(angle), Math.sin(angle));
-};
