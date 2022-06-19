6917dfff4118af2999bcb26bd5b6920e791b9a45
danny43861 <danny43861@gmail.com>
2022-06-19 18:09:40 +0800

    Add fight player

---
+++ assets/Scripts/fight/fightManager.ts
@@ -0,0 +1,200 @@
+// Learn TypeScript:
+//  - https://docs.cocos.com/creator/manual/en/scripting/typescript.html
+// Learn Attribute:
+//  - https://docs.cocos.com/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
+
+import fightPlayer from "./fightPlayer";
+const { ccclass, property } = cc._decorator;
+const keyboardInput = {};
+
+@ccclass
+export default class gameManager extends cc.Component {
+    @property(cc.Prefab)
+    archer: cc.Prefab = null;
+    @property(cc.Prefab)
+    warrior: cc.Prefab = null;
+    @property(cc.Prefab)
+    knight: cc.Prefab = null;
+    @property(cc.Prefab)
+    slinger: cc.Prefab = null;
+
+    @property(cc.AudioClip)
+    bgm: cc.AudioClip = null;
+    @property(cc.AudioClip)
+    windcutSound: cc.AudioClip = null;
+    @property(cc.AudioClip)
+    walkSound: cc.AudioClip = null;
+    
+    private player1: fightPlayer = null;
+    private player2: fightPlayer = null;
+
+    private map: cc.TiledMap = null;
+    private pause: boolean = false;
+    private physicManager: cc.PhysicsManager = null;
+
+    private passControl: number = 0;
+    //1: player1 pass first
+    //2: player2 pass first
+    //3: both player pass
+
+    // LIFE-CYCLE CALLBACKS:
+
+    onLoad() {
+        this.physicManager = cc.director.getPhysicsManager();
+        this.physicManager.enabled = true;
+        this.initPlayer();
+        let level = cc.sys.localStorage.getItem("level");
+    }
+
+    start() {
+        this.playBGM();
+        cc.systemEvent.on(cc.SystemEvent.EventType.KEY_DOWN, this.onKeyDown, this);
+        cc.systemEvent.on(cc.SystemEvent.EventType.KEY_UP, this.onKeyUp, this);
+
+        this.initWall(this.map);
+        
+    }
+    playBGM(){
+        cc.audioEngine.play(this.bgm, true, 0.5);
+    }
+    initPlayer() {
+        let p1Info = JSON.parse(cc.sys.localStorage.getItem("p1"))
+        var p1: cc.Node, p2: cc.Node;
+        switch (p1Info.job) {
+            case ('archer'):
+                p1 = cc.instantiate(this.archer);
+                break;
+            case ('warrior'):
+                p1 = cc.instantiate(this.warrior);
+                break;
+            case ('slinger'):
+                p1 = cc.instantiate(this.slinger);
+                break;
+            case ('knight'):
+                p1 = cc.instantiate(this.knight);
+                break;
+        
+        }
+        p1.parent = cc.find('Canvas/player1');
+        p1.setPosition(cc.v2(335, -55))
+
+        let p2Info = JSON.parse(cc.sys.localStorage.getItem("p2"))
+        switch (p2Info.job) {
+            case ('archer'):
+                p2 = cc.instantiate(this.archer);
+                break;
+            case ('warrior'):
+                p2 = cc.instantiate(this.warrior);
+                break;
+            case ('slinger'):
+                p2 = cc.instantiate(this.slinger);
+                break;
+            case ('knight'):
+                p2 = cc.instantiate(this.knight);
+                break;
+        }
+        p2.parent = cc.find('Canvas/player2');
+        p2.setPosition(cc.v2(335, -55))
+        this.player1 = p1.getComponent(fightPlayer);
+        this.player2 = p2.getComponent(fightPlayer);
+    }
+
+    initWall(map: cc.TiledMap) {
+        let tiledSize = map.getTileSize();
+        let layer = map.getLayer("wall");
+        let layerSize = layer.getLayerSize();
+        //check each tiled
+        for (let i = 0; i < layerSize.width; i++) {
+            for (let j = 0; j < layerSize.height; j++) {
+                let tiled = layer.getTiledTileAt(i, j, true);
+                if (tiled.gid != 0) {
+                    tiled.node.group = "wall";
+                    let body = tiled.node.addComponent(cc.RigidBody);
+                    body.type = cc.RigidBodyType.Static;
+                    let collider = tiled.node.addComponent(cc.PhysicsBoxCollider);
+                    collider.offset = cc.v2(tiledSize.width / 2, tiledSize.height / 2);
+                    collider.size = tiledSize;
+                    collider.apply();
+                }
+            }
+        }
+    }
+    onKeyDown(event) {
+        keyboardInput[event.keyCode] = 1;
+    }
+
+    onKeyUp(event) {
+        keyboardInput[event.keyCode] = 0;
+    }
+
+    keyboardUpdate() {
+        if (this.pause) return;
+        this.player1.playerMoveDir("IDLE");
+        this.player2.playerMoveDir("IDLE");
+        if (keyboardInput[cc.macro.KEY.space]) this.player1.playerAttack();
+        if (keyboardInput[cc.macro.KEY.m]) this.player1.playerPower();
+        if (keyboardInput[cc.macro.KEY.s] && keyboardInput[cc.macro.KEY.d]) {
+            this.player1.playerMoveDir("SE");
+        } else if (keyboardInput[cc.macro.KEY.d] && keyboardInput[cc.macro.KEY.w]) {
+            this.player1.playerMoveDir("NE");
+        } else if (keyboardInput[cc.macro.KEY.w] && keyboardInput[cc.macro.KEY.a]) {
+            this.player1.playerMoveDir("NW");
+        } else if (keyboardInput[cc.macro.KEY.a] && keyboardInput[cc.macro.KEY.s]) {
+            this.player1.playerMoveDir("SW");
+        } else {
+            if (keyboardInput[cc.macro.KEY.a]) {
+                this.player1.playerMoveDir("W");
+            } else if (keyboardInput[cc.macro.KEY.s]) {
+                this.player1.playerMoveDir("S");
+            } else if (keyboardInput[cc.macro.KEY.d]) {
+                this.player1.playerMoveDir("E");
+            } else if (keyboardInput[cc.macro.KEY.w]) {
+                this.player1.playerMoveDir("N");
+            }
+        }
+        if (keyboardInput[cc.macro.KEY.forwardslash]) this.player2.playerAttack();
+
+        if (keyboardInput[cc.macro.KEY.down] && keyboardInput[cc.macro.KEY.right]) {
+            this.player2.playerMoveDir("SE");
+        } else if (
+            keyboardInput[cc.macro.KEY.right] &&
+            keyboardInput[cc.macro.KEY.up]
+        ) {
+            this.player2.playerMoveDir("NE");
+        } else if (
+            keyboardInput[cc.macro.KEY.up] &&
+            keyboardInput[cc.macro.KEY.left]
+        ) {
+            this.player2.playerMoveDir("NW");
+        } else if (
+            keyboardInput[cc.macro.KEY.left] &&
+            keyboardInput[cc.macro.KEY.down]
+        ) {
+            this.player2.playerMoveDir("SW");
+        } else {
+            if (keyboardInput[cc.macro.KEY.up]) {
+                this.player2.playerMoveDir("N");
+            } else if (keyboardInput[cc.macro.KEY.down]) {
+                this.player2.playerMoveDir("S");
+            } else if (keyboardInput[cc.macro.KEY.right]) {
+                this.player2.playerMoveDir("E");
+            } else if (keyboardInput[cc.macro.KEY.left]) {
+                this.player2.playerMoveDir("W");
+            }
+        }
+    }
+
+    gameOver() {
+        cc.find("Canvas/loadingCamera").active = true;
+        cc.find("Canvas/tmp_bg").active = true;
+        cc.director.loadScene("start");
+    }
+
+    update(dt) {
+        this.keyboardUpdate();
+    }
+
+
+}
---
+++ assets/Scripts/fight/fightPlayer.ts
@@ -0,0 +1,333 @@
+const { ccclass, property } = cc._decorator;
+
+@ccclass
+export default class fightPlayer extends cc.Component {
+
+    @property(cc.Prefab)
+    private bulletPrefab: cc.Prefab = null;
+
+    @property(cc.Prefab)
+    private powerBulletprefab: cc.Prefab = null;
+
+    private moveDir: string = 'S';
+    private aimDir: cc.Vec2 = cc.v2(0, 0);
+    private speed: number = 250;
+
+    private anim: cc.Animation = null;
+    private animateState = null;
+
+    private maxBullet: number = 5;
+    private bulletPool: cc.NodePool = null;
+    private powerBulletpool: cc.NodePool = null;
+    private nextAttackTime: number = 0;
+    private nextReloadTime: number = 0;
+    private attackCooldown: number = 0.5;
+    private powerCooltime: number= 5;
+    private reloadCooldown: number = 5;
+    private attackRange: number = 300;
+    private attacking: boolean = false;
+    private rangeTarget: object = {};
+
+    private powerCooldown: boolean = false;
+
+    private targetNode: cc.Node = null;
+    private targetPosition: cc.Vec2 = cc.v2(0, 0);
+    private targetDirection: string = '';
+    private targetAngle: number = 0;
+
+    private life: number = 10;
+    private lifeMax: number = 10;
+    private lifeprogress: cc.Node = null;
+    private powerprogress: cc.Node = null;
+    private colorOfpower: cc.Node= null;
+
+    private characterName: string = 'archer';
+    private characterTag: number = 0;
+
+    private otherPlayerIndex: string = '0'
+
+
+    onLoad() {
+        this.bulletPool = new cc.NodePool('bullet');
+        this.powerBulletpool= new cc.NodePool('powerBullet');
+        this.anim = this.getComponent(cc.Animation);
+        this.lifeprogress = this.node.getChildByName('lifeBar');
+        this.powerprogress = this.node.getChildByName('powerBar');
+        this.colorOfpower= this.powerprogress.getChildByName('bar');
+        let index = this.node.parent.name.slice(-1);
+        if(index == '0') 
+            this.otherPlayerIndex = '1';
+        else 
+            this.otherPlayerIndex = '0'
+        try {
+            let currentCharacter = JSON.parse(cc.sys.localStorage.getItem("p" + index)).job;
+            if (currentCharacter) {
+                this.characterName = currentCharacter;
+                switch (currentCharacter) {
+                    case 'archer':
+                        this.characterTag = 0;
+                        break;
+                    case 'slinger':
+                        this.characterTag = 1;
+                        break;
+                    case 'warrior':
+                        this.characterTag = 2;
+                        break;
+                    case 'knight':
+                        this.characterTag = 3;
+                        break;
+                }
+            }
+        }
+        catch {
+
+        };
+    }
+
+    start() {
+        this.targetNode = cc.find("Canvas/player" + this.otherPlayerIndex)
+        for (let i: number = 0; i < this.maxBullet; i++) {
+            let bullet = cc.instantiate(this.bulletPrefab);
+            this.bulletPool.put(bullet);
+        }
+        let powerBullet= cc.instantiate(this.powerBulletprefab);
+        this.powerBulletpool.put(powerBullet);
+
+        if (this.lifeprogress == null) cc.log("fail");
+        else this.lifeprogress.getComponent(cc.ProgressBar).progress = 1;
+        this.powerprogress.getComponent(cc.ProgressBar).progress = 1;
+        this.colorOfpower.color=new cc.Color(0, 255, 80);
+    }
+
+    playerMoveDir(dir: string) {
+        this.moveDir = dir;
+    }
+
+    playerMove(dt: number) {
+        switch (this.moveDir) {
+            case 'N':
+                this.aimDir = cc.v2(0, 1)
+                this.node.y += this.speed * dt;
+                break;
+            case 'S':
+                this.aimDir = cc.v2(0, -1)
+                this.node.y -= this.speed * dt;
+                break;
+            case 'E':
+                this.aimDir = cc.v2(1, 0)
+                this.node.x += this.speed * dt;
+                break;
+            case 'W':
+                this.aimDir = cc.v2(-1, 0)
+                this.node.x -= this.speed * dt;
+                break;
+            case 'NE':
+                this.aimDir = cc.v2(1, 1)
+                this.node.x += this.speed * dt;
+                this.node.y += this.speed * dt;
+                break;
+            case 'NW':
+                this.aimDir = cc.v2(-1, 1)
+                this.node.x -= this.speed * dt;
+                this.node.y += this.speed * dt;
+                break;
+            case 'SW':
+                this.aimDir = cc.v2(-1, -1)
+                this.node.x -= this.speed * dt;
+                this.node.y -= this.speed * dt;
+                break;
+            case 'SE':
+                this.aimDir = cc.v2(1, -1)
+                this.node.x += this.speed * dt;
+                this.node.y -= this.speed * dt;
+                break;
+
+        }
+    }
+
+    playerWalkAnimation() {
+        if (this.attacking)
+            return;
+
+        switch (this.moveDir) {
+            case 'N':
+                if (this.animateState == null || this.animateState.name != this.characterName + 'WalkN')
+                    this.animateState = this.anim.play(this.characterName + 'WalkN');
+                break;
+            case 'S':
+                if (this.animateState == null || this.animateState.name != this.characterName + 'WalkS')
+                    this.animateState = this.anim.play(this.characterName + 'WalkS');
+                break;
+            case 'E':
+                if (this.animateState == null || this.animateState.name != this.characterName + 'WalkE')
+                    this.animateState = this.anim.play(this.characterName + 'WalkE');
+                break;
+            case 'W':
+                if (this.animateState == null || this.animateState.name != this.characterName + 'WalkW')
+                    this.animateState = this.anim.play(this.characterName + 'WalkW');
+                break;
+            case 'NE':
+                if (this.animateState == null || this.animateState.name != this.characterName + 'WalkNE')
+                    this.animateState = this.anim.play(this.characterName + 'WalkNE');
+                break;
+            case 'NW':
+                if (this.animateState == null || this.animateState.name != this.characterName + 'WalkNW')
+                    this.animateState = this.anim.play(this.characterName + 'WalkNW');
+                break;
+            case 'SW':
+                if (this.animateState == null || this.animateState.name != this.characterName + 'WalkSW')
+                    this.animateState = this.anim.play(this.characterName + 'WalkSW');
+                break;
+            case 'SE':
+                if (this.animateState == null || this.animateState.name != this.characterName + 'WalkSE')
+                    this.animateState = this.anim.play(this.characterName + 'WalkSE');
+                break;
+            default:
+                this.animateState = null
+                this.anim.stop();
+                break;
+        }
+    }
+
+    playerAttackAnimation() {
+        this.anim.stop();
+        this.attacking = true;
+        this.animateState = this.anim.play(this.characterName + 'Attack' + this.targetDirection);
+        this.anim.on('finished', (e) => {
+            this.attacking = false;
+        })
+    }
+
+    playerAttack() {
+        this.traceTarget();
+        let currentTime = cc.director.getTotalTime() / 1000.0;
+        if (currentTime >= this.nextAttackTime) {
+            this.nextAttackTime = currentTime + this.attackCooldown;
+            if(this.characterTag == 0 || this.characterTag == 1){
+                this.createBullet();
+            }
+            else{ 
+                this.meleeAttack();
+            }
+            this.playerAttackAnimation();
+        }
+    }
+
+    playerPower() {
+        if (!this.powerCooldown) {
+            this.powerCooldown = true;
+            this.powerprogress.getComponent(cc.ProgressBar).progress=0;
+            this.colorOfpower.color=new cc.Color(25, 95, 25);
+            if(this.characterTag==0){
+                this.schedule(() => {
+                    this.traceTarget();
+                    this.createBullet();
+                    this.playerAttackAnimation();
+                }, 0.2, 4)
+            }
+            else if(this.characterTag==1){
+                this.traceTarget();
+                let powerBullet=null;
+                if(this.powerBulletpool.size()>0) powerBullet=this.powerBulletpool.get(this.powerBulletpool); 
+                if(powerBullet != null) powerBullet.getComponent('powerBullet').init(this.node, this.targetDirection, this.targetAngle);
+
+                this.playerAttackAnimation();
+            }
+
+            else if(this.characterTag==2){
+                this.attackCooldown=0.25;
+                this.node.color=new cc.Color(248, 86, 86);
+                this.scheduleOnce(() => {
+                    this.attackCooldown=0.5;
+                    this.node.color=new cc.Color(255, 255, 255);
+                }, 2.5)
+            }
+
+            else if(this.characterTag==3){
+                this.node.color=new cc.Color(134, 250, 255);
+                this.scheduleOnce(() => {
+                    this.node.color=new cc.Color(255, 255, 255);
+                }, 2.5)
+            }
+            this.scheduleOnce(() => {
+                this.setPowerCooldown(0);
+                this.colorOfpower.color=new cc.Color(0, 255, 80);
+            }, this.powerCooltime)
+        }
+    }
+
+    setPowerCooldown(num) {
+        if (!num) this.powerCooldown = false;
+    }
+
+    createBullet() {
+        let bullet = null;
+        if (this.bulletPool.size() > 0) {
+            bullet = this.bulletPool.get(this.bulletPool);
+        }
+
+        if (bullet != null)
+            bullet.getComponent('bullet').init(this.node, this.targetDirection, this.targetAngle);
+    }
+
+    meleeAttack() {
+        let currentPos = this.node.convertToWorldSpaceAR(cc.v3(0, 0, 0));
+        let otherPos = this.targetNode.convertToWorldSpaceAR(cc.v3(0, 0, 0));
+        let diff = cc.v2(otherPos.x - currentPos.x, otherPos.y - currentPos.y)
+        if (diff.mag() < this.attackRange)
+            this.targetNode.getComponent('fightPlayer').lifeDamage(1);
+    }
+
+    traceTarget() {
+        let currentPos = this.node.convertToWorldSpaceAR(cc.v3(0, 0, 0));
+        let otherPos = this.targetNode.convertToWorldSpaceAR(cc.v3(0, 0, 0));
+
+        let diff = cc.v2(otherPos.x - currentPos.x, otherPos.y - currentPos.y)
+        if (diff.mag() > this.attackRange)
+            this.targetPosition = cc.v2(currentPos.x + this.aimDir.x * this.attackRange, currentPos.y + this.aimDir.y * this.attackRange);
+        else 
+            this.targetPosition = cc.v2(otherPos.x, otherPos.y);
+        
+        this.targetAngle = calcAngleDegrees(this.targetPosition.x - currentPos.x, this.targetPosition.y - currentPos.y);
+        if (this.targetAngle < 157.5 && this.targetAngle >= 112.5) {
+            this.targetDirection = 'NW';
+        }
+        else if (this.targetAngle < 112.5 && this.targetAngle >= 67.5) {
+            this.targetDirection = 'N'
+        }
+        else if (this.targetAngle < 67.5 && this.targetAngle >= 22.5) {
+            this.targetDirection = 'NE';
+        }
+        else if (this.targetAngle < 22.5 && this.targetAngle >= -22.5) {
+            this.targetDirection = 'E';
+        }
+        else if (this.targetAngle < -22.5 && this.targetAngle >= -67.5) {
+            this.targetDirection = 'SE';
+        }
+        else if (this.targetAngle < -67.5 && this.targetAngle >= -112.5) {
+            this.targetDirection = 'S';
+        }
+        else if (this.targetAngle < -112.5 && this.targetAngle >= -157.5) {
+            this.targetDirection = 'SW';
+        }
+        else {
+            this.targetDirection = 'W';
+        }
+    }
+
+    lifeDamage(damage: number) {
+        if (this.life > 0) this.life -= damage;
+    }
+
+    update(dt) {
+        this.playerMove(dt);
+        this.playerWalkAnimation();
+        this.lifeprogress.getComponent(cc.ProgressBar).progress = this.life / this.lifeMax;
+        if(this.powerCooldown) this.powerprogress.getComponent(cc.ProgressBar).progress+=dt/this.powerCooltime;
+    }
+}
+
+
+function calcAngleDegrees(x, y) {
+    return Math.atan2(y, x) * 180 / Math.PI;
+}
